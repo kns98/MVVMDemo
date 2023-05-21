e857044b6d2d7a3e1a5fb10ab0720a756205a668
kns98 <kns98@yahoo.com>
2023-05-21 16:18:17 +0800

    comments

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,42 +3,59 @@ using System.ComponentModel;
 
 Console.WriteLine("Hello, World!");
 
-UserVM u = new UserVM();
+UserViewModel u = new UserViewModel();
 
-u.PropertyChanged += U_PropertyChanged;
+u.PropertyChanged += U_PropertyChanged; // add as many events as you need
+u.PropertyChanged += U_Celebrate;
+
+//
+//NB: We have not discussed how the Model fits in yet. Only View Model and View.
+//In XAML MVVM = Extensible Application Markup Language using a Model-View-ViewModel paramdign
+//Binding syntax is used to propagate events
+//
 
 void U_PropertyChanged(object? sender, PropertyChangedEventArgs e)
 {
-    Console.WriteLine(e.PropertyName + " was changed to " + ((UserVM)sender).Name);
+    Console.WriteLine(e.PropertyName + " was changed to " + ((UserViewModel)sender).Age);
 }
 
-u.Name = "Kevin";
-u.Name = "Kevin";
-u.Name = "Joe";
+void U_Celebrate(object? sender, PropertyChangedEventArgs e)
+{
+    Console.WriteLine("Hurrah!");
+}
 
+u.Age = "45";
+u.Age = "46";
+u.Age = "46.1";
 
-class UserVM : INotifyPropertyChanged
+Console.WriteLine("Finished");
+Console.ReadLine();
+
+class UserViewModel : INotifyPropertyChanged // standardization -- once we have it, can be in MVVM apps.
 {
     public event PropertyChangedEventHandler? PropertyChanged;
 
     void handler(object o, PropertyChangedEventArgs a) { 
-        //dynamic as event(s) may not be in place
+        //
+        // dynamic as event(s) may not be in place - that's why we need question mark
+        // do more in depth on events if needed
+        //
         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(a.PropertyName));
     }
 
-    string name = "Hello";
+    string age = "0";
 
-    public string Name { 
-        get { return name; }
+    public string Age { 
+        get { return age; }
         set
         {
-            name = value;
-            handler(this, new PropertyChangedEventArgs("Name"));
+            age = value;
+            handler(this, new PropertyChangedEventArgs("Age"));
         }
         
     }
 
-    public UserVM()
+    public UserViewModel()
     {
 
     }
